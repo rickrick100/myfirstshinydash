9dc14f04a600b868d0221a95da35dd28e87a1748
rickrick100 <rengarick@gmail.com>
2018-02-12 11:37:53 +0800

    Add files via upload

--- graph.R
+++ graph.R
@@ -10,11 +10,12 @@ library(rsconnect)
 
 
 s <- read_csv("s.csv")
-s$shooting_incidents_total[s$shooting_incidents_total=="-"] <- 0 
+s$shooting_incidents_total[s$shooting_incidents_total=="-"] <- 0
 s$robbery_total[s$robbery_total=="-"] <- 0
 s$carnapping_total[s$carnapping_total=="-"] <- 0
 s$others_total[s$others_total=="-"] <- 0
 s$total_number_of_incidents[s$total_number_of_incidents=="-"] <- 0
+
 totalByYear <- s %>%
   select(year,shooting_incidents_total,robbery_total,carnapping_total,
          others_total,total_number_of_incidents)%>%
@@ -28,8 +29,6 @@ totalByYear <- s %>%
             )
 
 totalByYear1 <- melt(totalByYear,id.var="year")
-
-
 
 totalByYear2<- ggplot(totalByYear1,mapping = aes(year,value,fill=variable))+
   geom_histogram(stat="identity")+facet_grid(~variable)+
@@ -50,13 +49,13 @@ totalByYear3<- ggplot(totalByYear1,mapping = aes(year,value))+
                       labels=c("Other Crimes  ","Total Carnapping  ",
                               "Total Shooting  ","Total Robbery  ","Total Crime"))
 
-totalByYear3
 perRegion <- s %>%
   select(police_regional_office,total_number_of_incidents)%>%
   filter(total_number_of_incidents>=0)%>%
   group_by(police_regional_office)%>%
   summarise(sumPerRegion= sum(as.numeric(total_number_of_incidents)))%>%
   arrange(sumPerRegion)
+
 perRegion2 <- ggplot(perRegion,mapping = aes(police_regional_office,
                                              sumPerRegion,fill=police_regional_office))+
   geom_histogram(stat="identity")+
@@ -69,5 +68,4 @@ perRegion2 <- ggplot(perRegion,mapping = aes(police_regional_office,
   ggtitle("Total Crimes Related to Riding in Tandem: Per Region (2011-2013)")+
   theme_bw()+theme(plot.title = element_text(hjust = 0.5,face="bold",size=15))+
   theme(axis.title=element_text(face="bold",size=15))+theme(text=element_text(size=15))
-perRegion2
 
